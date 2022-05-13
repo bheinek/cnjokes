2366d0cf63016acf7c3917a6ff660990349af1da
Hynek <bagar@cngroup.dk>
2022-05-13 12:24:00 +0200

    solved responsivity (no scrolling menu on small screen) + config prettier

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
-import '../App.css';
+import '../Menu.css';
 import {Menu} from './Menu' 
 
 export function Header (){
 return (
 <header className="header">
-<Menu />
+    <Menu/>
     <h1><a className='title' href='.'>CN Jokes</a></h1>
 <p className="description">Website dedicated to facts about the greatest person in human history -  Mr. Chuck Norris </p>
 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,22 +1,25 @@
-import "../App.css";
+import "../Menu.css";
 export function Menu() {
   return (
-    <div className="menuBar hidden-sm">
-      <a href=".">Categorie 1</a>
-      <a href=".">Categorie 2</a>
-      <a href=".">Categorie 3</a>
-      <a href=".">Categorie 4</a>
-      <a href=".">Categorie 5</a>
-      <a href=".">Categorie 6</a>
-      <a href=".">Categorie 8</a>
-      <a href=".">Categorie 9</a>
-      <a href=".">Categorie 10</a>
-      <a href=".">Categorie 11</a>
-      <a href=".">Categorie 12</a>
-      <a href=".">Categorie 13</a>
-      <a href=".">Categorie 14</a>
-      <a href=".">Categorie 15</a>
-      <a href=".">Categorie 16</a>
-    </div>
+    <nav className="navbar">
+      <ul className="nav-links">
+      <input type="checkbox" id="checkbox-toggle"/>
+      <label for="checkbox-toggle" class="hamburger">&#9776;</label>
+      <div className="menu">
+      <li><a href=".">Categorie 1</a></li>
+      <li><a href=".">Categorie 1</a></li>
+      <li><a href=".">Categorie 1</a></li>
+      <li><a href=".">Categorie 1</a></li>
+      <li><a href=".">Categorie 1</a></li>
+      <li><a href=".">Categorie 1</a></li>
+      <li><a href=".">Categorie 1</a></li>
+      <li><a href=".">Categorie 1</a></li>
+      <li><a href=".">Categorie 1</a></li>
+      <li><a href=".">Categorie 1</a></li>
+      <li><a href=".">Categorie 1</a></li>
+     
+      </div>
+      </ul>
+    </nav>
   );
 }
