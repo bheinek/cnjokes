5512b062ad58cc134489d683db8540ed6d2623be
Hynek <bagar@cngroup.dk>
2022-05-16 10:19:49 +0200

    working fetching for categories data

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
-import { useState, useEffect } from 'react';
+
 import './App.css';
+import {Data} from "./components/Data"
 
 function App() {
-fetch(`https://api.chucknorris.io/jokes/categories`,"GET" )
-const [data, setData] = useState(null);
-const [error, setError] = useState(null);
-  
-    useEffect(() => {
-      fetch(`https://api.chucknorris.io/jokes/categories`)
-      .then((response) => {
-        if(!response){
-          throw new Error(
-            `This is an HTTP error: The status is ${response.status}`
-          );
-        }
-        return response.json()})
-      .then((actualData) => {
-        setData(actualData);
-        setError(null)})
-      .catch((err)=>{
-        setError(err.message);
-        setData(null);
-      })
-     }, [])
-    
-
-     return (
-       <div>
-         <ul>
-        {data &&
-          data.map((category) => (
-            <li>
-              <h3>{category}</h3>
-            </li>
-          ))}
-      </ul>
-
-       </div>
-     )
+  return(
+<div>
+  <Data />
+</div>
+)
 }
 
 export default App;
