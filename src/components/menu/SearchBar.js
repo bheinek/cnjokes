8f3b48f30d8fad59da243098dac9abb396861769
Hynek <bagar@cngroup.dk>
2022-05-25 15:50:06 +0200

    fixed submit handle and or option

--- src/components/menu/SearchBar.js
+++ src/components/menu/SearchBar.js
@@ -4,29 +4,29 @@ import { Button, Form, Input } from 'reactstrap';
 
 import './Menu.css';
 
-export function SearchBar({ setButton, setValue, ...rest }) {
+export function SearchBar() {
   const [search, setSearch] = useState('');
   const navigate = useNavigate();
-  const onSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     if (!search) {
       return alert('Please write what you are searching for!');
     }
-    if ((search.length < 3) | (search.length > 120))
+    if (search.length < 3 || search.length > 120)
       return alert('The search must contain between 3 and 120 letters');
     navigate(`/search/${search}`);
     setSearch('');
   };
 
   return (
-    <Form id="search" onSubmit={onSubmit}>
+    <Form id="search" onSubmit={handleSubmit}>
       <Input
         type="text"
         value={search}
         onChange={(event) => setSearch(event.target.value)}
         placeholder="Vyhledat vtip ..."
       />
-      <Button onSubmit={onSubmit}>Search</Button>
+      <Button type="submit">Search</Button>
     </Form>
   );
 }
